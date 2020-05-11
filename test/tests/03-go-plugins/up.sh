20d4569c4ad8fb06c99cf94dff4172b26de42b04
Colin Hutchinson <chutchic@gmail.com>
2020-05-11 23:52:55 +0000

    fix(go) small tweak to ensure building

--- test/tests/03-go-plugins/up.sh
+++ test/tests/03-go-plugins/up.sh
@@ -2,8 +2,8 @@
 # clone plugins
 git clone https://github.com/Kong/go-plugins
 pushd go-plugins
-
   echo "Go builder image: $DOCKER_GO_BUILDER"
+  rm -rf go-plugins/*.so
 
   for src in *.go; do
     echo "compile $src"
@@ -11,19 +11,6 @@ pushd go-plugins
   done
 
 popd
-
-# cp -v *.so /usr/local/kong
-# cd ..
-# rm -rf go-plugins
-
-# docker build --build-arg DOCKER_GO_BUILDER=$DOCKER_GO_BUILDER \
-#   -f Dockerfile.build_plugin -t go-plugin-builder .
-# docker run --name go-plugin-builder-container go-plugin-builder
-
-# docker cp go-plugin-builder-container:/go-plugins/go-hello.so .
-
-# docker rm -f go-plugin-builder-container
-# rm -rf go-plugins
 
 echo $KONG_TEST_IMAGE_NAME
 docker build --build-arg KONG_TEST_IMAGE_NAME=$KONG_TEST_IMAGE_NAME \
